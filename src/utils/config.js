d09893cea6fa3c5bfb8c3bb4c2d0876cbf5f3039
shubham banga <shubham@softelevation.com>
2022-11-25 17:09:51 +0530

    change url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 export const config = {
-  Api_Url: 'https://api.beontime.io',
+  // Api_Url: 'https://api.beontime.io',
+  Api_Url: 'https://api.beontime.softelevation.com',
   googleKey: 'AIzaSyBf4G3qQTDy6-DN6Tb9m6WzgYCW598EoxU',
 };
